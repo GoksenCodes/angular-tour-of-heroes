a76b0077a518a60007905167facda7d1fff93972
Goksen <goksengorgulu@gmail.com>
2020-07-30 12:40:54 +0200

    display the message form HeroService by binding messages component template to the message service

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-messages',
@@ -6,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./messages.component.css']
 })
 export class MessagesComponent implements OnInit {
-  constructor() {}
+  constructor(public messageService: MessageService) {}
+  //messageservice property needs to be public beacuse we will bind it in the template
 
   ngOnInit(): void {}
 }
