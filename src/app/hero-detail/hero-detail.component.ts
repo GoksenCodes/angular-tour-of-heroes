e3a679b6cf8e770ee3e77ab96ca1b37acfd5f3a7
Goksen <goksengorgulu@gmail.com>
2020-07-30 11:30:46 +0200

    create hero detail component & move hero details to this component

---
+++ src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css']
+})
+export class HeroDetailComponent implements OnInit {
+  @Input() hero: Hero;
+  // to be able to make hero property accessible by other component , it must be an Input property
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
